fbaeb599cbbee90c4751e7c1a433bc3187077cff
abhinav bansal <abhinavbansal@chegg.com>
2018-10-16 17:02:09 +0530

    3 rd commit

--- botchat/helpers/processMessage.js
+++ botchat/helpers/processMessage.js
@@ -12,7 +12,53 @@ const sendTextMessage = (senderId, text) => {
  }
  });
 };
+const sendImageMessage = (senderId, text) => {
+	console.log("ahsghg");
+ request({
+ url: 'https://graph.facebook.com/v2.6/me/messages',
+ qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+ method: 'POST',
+ json: {
+ recipient: { id: senderId },
+ message: { "attachment":{
+      "type":"image", 
+      "payload":{
+        "is_reusable": true,
+        "url":text
+      }
+    } },
+ }
+ });
+};
 
+const sendTypingMessage = (senderId,actionq) => {
+	console.log("ahsghg");
+ request({
+ url: 'https://graph.facebook.com/v2.6/me/messages',
+ qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+ method: 'POST',
+ json: {
+ recipient: { id: senderId },
+ sender_action: actionq,
+ }
+ });
+};
+
+function makeImageRequest(image_endpoint,senderId){
+request(image_endpoint,function(err,response, body){
+
+	            if (err){
+                console.log(err);}
+            else {
+
+                var data = JSON.parse(body);
+            	console.log(data);
+
+            	sendImageMessage(senderId,data[0].imageUrl);
+            }
+
+});
+}
 function getLuisIntent(senderId, utterance) {
 
 var endpoint = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/1744c2b5-be97-4816-820c-617c812e199c?subscription-key=0e079552e3fa4302bb49b4ea78809cfc&spellCheck=true&bing-spell-check-subscription-key=0e079552e3fa4302bb49b4ea78809cfc&timezoneOffset=-360&q='+ utterance;
@@ -29,16 +75,28 @@ request(endpoint,
                 var data = JSON.parse(body);
             	console.log(data);
 
- 
-sendTextMessage(senderId,data.entities[0].type);
+ for (prop in data.entities)
+ {
+ 	console.log(data.entities[prop]);
+
+ }
+ var pressure ='pressure';
+var image_endpoint= 'http://91463b41.ngrok.io/rest/image/getImages?query='+pressure+'&index=images';
+makeImageRequest(image_endpoint,senderId);
+//sendTextMessage(senderId,data.entities[0].type);
 
             }
+
+
         });
 };
 module.exports = (event) => {
+
 	console.log("asasas");
  const senderId = event.sender.id;
  const message = event.message.text;
+ sendTypingMessage(senderId,'mark_seen');
+ sendTypingMessage(senderId,'typing_on');
              	 returnMessage = getLuisIntent(senderId,message);
  		
  
--- botchat/index.js
+++ botchat/index.js
@@ -10,4 +10,8 @@ app.listen(3008, () => console.log('Webhook server is listening, port 3000'));
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
 app.get('/', verificationController);
+
 app.post('/', messageWebhookController);
+app.get('/botpolicy', function(req, res) {
+    res.send('below are the policies');
+});
